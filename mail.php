762e06550e07c5f8c1d0d4d054d5ce218791ed37
deepaklogin <55199005+deepaklogin@users.noreply.github.com>
2020-01-24 13:10:11 +0530

    awesome
    nice mail file

---
+++ mail.php
@@ -0,0 +1,11 @@
+<!--
+function to send emails 
+mail('email id ', 'subject of mail' , 
+'body of mail ', 'header message of mail');
+
+!-->
+
+<?php
+mail('deepakparsabad2@gmail.com','welcome Deepak rajak','this mail is send by abc company','abc ltd');
+// mail('bindas1106@gmail.com','welcome mail','this mail is send by abc company','abc ltd');
+?>
